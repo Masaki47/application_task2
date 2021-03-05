4e5ec13214174ccefbced94652fd16823f2d711d
Masaki47 <mosnaakgi70411@gmail.com>
2021-03-05 15:12:18 +0000

    [Finish]task4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,9 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
-    @booknew = Book.new
-    @user = @book.user
-    @book_comment = BookComment.new
   end
 
   def index
@@ -20,26 +18,15 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
     else
-      @user = current_user
       @books = Book.all
       render :index
     end
   end
 
   def edit
-    @book = Book.find(params[:id])
-    @user = @book.user
-    if @user == current_user
-      render "edit"
-    else
-      redirect_to books_path
-    end
   end
 
-
-
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -48,7 +35,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
@@ -57,6 +43,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,27 @@ class User < ApplicationRecord
 
   has_many :books
   attachment :profile_image, destroy: false
-  has_many :favorites, dependent: :destroy
-  has_many :book_comments, dependent: :destroy
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+
+  def follow(user_id)
+  　relationships.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  # フォロー確認をおこなう
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
